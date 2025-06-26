4b0d545249c182323424c934e0f7d37fcc9cf6f4
Silva-NK <kiagonsuzanne@gmail.com>
2025-06-26 19:09:19 +0300

    Adds validation rules to the models.

--- server/models.py
+++ server/models.py
@@ -12,6 +12,23 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or name.strip() == "":
+            raise ValueError("Author name is required")
+        
+        existing = Author.query.filter_by(name=name).first()
+        if existing:
+            raise ValueError("Author name must be unique")
+        
+        return name
+    
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number or len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be exactly 10 digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +44,37 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('content')
+    def validate_content(self, key, content):
+        if not content or len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return content
+    
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Summary must be less than or equal to 250 characters.")
+        return summary
+    
+
+    @validates('category')
+    def validate_category(self, key, category):
+        valid_categories = ["Fiction", "Non-Fiction"]
+        if category not in valid_categories:
+            raise ValueError("Category must be 'Fiction' or 'Non-Fiction.")
+        return category
+    
+
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not title:
+            raise ValueError("Post title is required.")
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Post title must be clickbait.")
+        return title
 
 
     def __repr__(self):
